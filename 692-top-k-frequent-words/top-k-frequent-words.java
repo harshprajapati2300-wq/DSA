b28496f63e333d92e97135d5be4e299dace6f3f6
harshprajapati2300-wq <harshprajapati2300@gmail.com>
2026-08-16 22:54:27 +0530

    Time: 8 ms (57.63%) | Memory: 46.4 MB (86.98%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
+            if (map.get(a).equals(map.get(b))) {
+                return b.compareTo(a);
+            }
+            return map.get(a) - map.get(b);
+        });
+
+        for (String word : map.keySet()) {
+            pq.offer(word);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll());
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
